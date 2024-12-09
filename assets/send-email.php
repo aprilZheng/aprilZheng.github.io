da7576672110956b8ee1845178c54e1e41b1a53f
aprilZheng <zhengtonglin211@gmail.com>
2024-12-09 03:19:06 +0000

    email

--- assets/send-email.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    mail("april_mp@outlook.com", "New Message from $name", $message, "From: $email");
-}
-?>
